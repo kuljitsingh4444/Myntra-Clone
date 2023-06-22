3176cd7b42171a37c2ef163c8a2736f273f6e347
Kuljit <kuljit.singh4444@gmail.com>
2023-06-23 00:10:17 +0530

    ui for dropdown

--- src/components/common/dropdown/dropdown.js
+++ src/components/common/dropdown/dropdown.js
@@ -1,10 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import Select, { components } from "react-select";
 import "./dropdown.css";
 
 const Dropdown = ({ currentValue, prefix, options, onChange }) => {
+  const customStyles = {
+    control: (base) => ({
+      ...base,
+      borderColor: "rgb(212, 213, 217)",
+      boxShadow: 'none',
+      border: 'none'
+    }),
+    option: (styles, { isFocused, isSelected }) => ({
+      ...styles,
+      background: isFocused ? '#f5f5f6' : isSelected? '#f5f5f6': "unset",
+      color: "black",
+      zIndex: 1,
+      fontWeight: isSelected ? "bold" : "unset",
+    })
+  };
+
   return (
     <Select
+      styles={customStyles}
       value={currentValue}
       onChange={(e) => onChange(e)}
       className="sortby"
